649d67d950f5ad727cb928abb3004d786b2b5c8e
Elijah Lucian <elijahlucian@gmail.com>
2020-10-05 17:03:32 -0600

    🏘

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
+import * as dotenv from 'dotenv'
+import * as Discord from 'discord.js'
+dotenv.config()
+
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', (msg) => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!')
+  }
+  console.log(msg)
+})
+
+console.log(process.env.DISCORD_TOKEN)
+
+// client.login(env.DISCORD_TOKEN)
+
 console.log('hello bot')
