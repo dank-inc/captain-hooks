94b83110e228fa13bcbc94fdbfb7b829b0f2fbf8
Elijah Lucian <elijahlucian@gmail.com>
2020-10-05 17:28:43 -0600

    🧹

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,30 @@ import * as dotenv from 'dotenv'
 import * as Discord from 'discord.js'
 dotenv.config()
 
-const client = new Discord.Client()
-const { DISCORD_TOKEN } = process.env
+const bot = new Discord.Client()
+const { DISCORD_TOKEN, DISCORD_TEST_CHANNEL_ID } = process.env
 
-client.on('ready', () => {
-  console.log(`My Botty is ready! => ${client.user.tag}!`)
+bot.on('ready', () => {
+  // anything whne it's logged in
+  bot.user.setActivity('WITH MYSELF', { type: 'PLAYING' })
+
+  console.log(`My Botty is ready! => ${bot.user.tag}!`)
 })
 
-client.on('message', (msg) => {
+bot.on('message', (msg) => {
   if (msg.content === 'ping') {
     msg.reply('Pong!')
   }
-  console.log(msg)
+
+  console.log(msg.channel.id, msg.content)
 })
 
-console.log(client.channels.cache)
+const channels = bot.channels.cache.map((c) => c)
+
+console.log(channels)
 
 if (!DISCORD_TOKEN) {
   console.error('hey, you knob, put the discord token on the thing')
 }
 
-client.login(DISCORD_TOKEN)
+bot.login(DISCORD_TOKEN)
